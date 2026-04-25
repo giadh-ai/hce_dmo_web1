7a153236fe39e0817b5db57ee817b4d946640d57
giadh-ai <giadh@binhduong-aptech.edu.vn>
2026-04-25 07:46:39 +0700

    Add files via upload

---
+++ src/main/java/hce/web/hce_dmo_web1/HceDmoWeb1Application.java
@@ -0,0 +1,13 @@
+package hce.web.hce_dmo_web1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HceDmoWeb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HceDmoWeb1Application.class, args);
+	}
+
+}
---
+++ src/test/java/hce/web/hce_dmo_web1/HceDmoWeb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package hce.web.hce_dmo_web1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HceDmoWeb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
