f6ab507a687397c44db32b2196383aff1246b84a
giadh-ai <giadh@binhduong-aptech.edu.vn>
2026-04-25 03:34:37 +0000

    Ngay 25/4

--- src/main/java/hce/web/hce_dmo_web1/controller/ProductController.java
+++ src/main/java/hce/web/hce_dmo_web1/controller/ProductController.java
@@ -1,11 +1,13 @@
 package hce.web.hce_dmo_web1.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import hce.web.hce_dmo_web1.models.*;
-import java.util.*;
+
+import hce.web.hce_dmo_web1.models.Product;
+import hce.web.hce_dmo_web1.models.ProductAPI;
 
 @RestController
 @RequestMapping("/products")
